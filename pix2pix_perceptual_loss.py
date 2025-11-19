9e527aeff73ef1d1f6e2aff907125a42045bf678
cvcapelle-uu <c.vancapelle@students.uu.nl>
2025-11-19 14:17:47 +0100

    Fix indentation for lambda_perc argument

--- pix2pix_perceptual_loss.py
+++ pix2pix_perceptual_loss.py
@@ -55,8 +55,8 @@ class Pix2PixModel(BaseModel):
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
-	    parser.add_argument('--lambda_perc', type=float, default=1.0,
-            help='weight for perceptual loss')
+		parser.add_argument('--lambda_perc', type=float, default=1.0, 
+							help='weight for perceptual loss')
 
 
         return parser
